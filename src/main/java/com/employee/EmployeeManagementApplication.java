8c4cbdecf62aab1b5b9d57f9721c1dc8786ce887
Aniket Gauliwar <aniketgauliwar395@gmail.com>
2026-05-15 22:51:42 +0530

    changes

--- src/main/java/com/employee/EmployeeManagementApplication.java
+++ src/main/java/com/employee/EmployeeManagementApplication.java
@@ -1,2 +1,15 @@
 package com.employee;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the Employee Management REST API.
+ */
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
+}
